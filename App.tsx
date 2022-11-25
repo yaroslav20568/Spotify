b28e0aff20a0872363ed5bb123a2b65e61949f1d
Yaroslav <ravlushevich.iarek@mail.ru>
2022-11-25 21:26:20 +0300

    add splash-screen

--- App.tsx
+++ App.tsx
@@ -1,7 +1,13 @@
+import { useEffect } from 'react';
 import { View } from 'react-native';
+import SplashScreen from 'react-native-splash-screen'
 import Tabs from './src/navigation/Tabs';
 
 const App = () => {
+	useEffect(() => {
+		SplashScreen.hide();
+	}, []);
+
   return (
     <Tabs/>
   );
